cfafaca2bb25c7936706d91f77d80e1848d695d8
Daniel A. Torquato <daniel.torquato@icomp.ufam.edu.br>
2019-11-02 17:56:31 -0400

    Imp: change function name and arguments

--- C/Continous_Row_Binary/src/main.c
+++ C/Continous_Row_Binary/src/main.c
@@ -2,39 +2,37 @@
 #include <stdlib.h>
 
 
-int goto_next_cell(int **grid, int n, int m, int i, int j, int *flag, int *lvl)
+int find_conex_element(int **grid, int n, int m, int i, int j, int *flag)
 {
 	if (grid[i][j]<1)
 		return 0;
 
-	grid[i][j]=-1-*lvl;
+	grid[i][j]=-1;
 
 	if (i<n-1 && grid[i+1][j]>0) {
-		if (!*flag) {
+		if (!*flag)
 			*flag=1;
-		}
-		goto_next_cell(grid, n, m, i+1, j, flag, lvl);
+		find_conex_element(grid, n, m, i+1, j, flag);
 	}
 
 	if (j<m-1 && grid[i][j+1]>0) {
-		if (!(*flag)) {
+		if (!(*flag))
 			*flag=1;
-		}
-		goto_next_cell(grid, n, m, i, j+1, flag, lvl);
+		find_conex_element(grid, n, m, i, j+1, flag);
 	}
 
 	if ((i>0 && grid[i-1][j])>0) {
 		if (!(*flag)) {
 			*flag=1;
 		}
-		goto_next_cell(grid, n, m, i-1, j, flag, lvl);
+		find_conex_element(grid, n, m, i-1, j, flag);
 	}
 
 	if (j>0 && grid[i][j-1]>0) {
 		if (!(*flag)) {
 			*flag=1;
 		}
-		goto_next_cell(grid, n, m, i, j-1, flag, lvl);
+		find_conex_element(grid, n, m, i, j-1, flag);
 	}
 }
 
@@ -46,14 +44,10 @@ int get_cont(int **grid, int n, int m)
 		for(j=0; j<m; j++) {
 			if (grid[i][j]>0) {
 				ret++;
-				goto_next_cell(grid, n, m, i, j, &flag, &ret);
+				find_conex_element(grid, n, m, i, j, &flag);
 			}
 			flag=0;
-#ifdef DEBUG
-			printf("%d ", ret);
-#endif
 		}
-			//check_adjacence_cell(grid, &ret, i, j, n, m);
 
 	return ret;
 }
@@ -61,37 +55,18 @@ int get_cont(int **grid, int n, int m)
 void alloc_matrix (int ***matrix, int n, int m)
 {
 	*matrix=(int **) malloc(n*sizeof(int*));
-#ifdef MEM_DEBUG
-	printf("%d\n", *matrix);
-#endif
 	int i;
 	for (i=0; i<n; i++) {
 		(*matrix)[i]=(int *) malloc(m*sizeof(int));
-#ifdef MEM_DEBUG
-		printf("%d ", (*matrix)[i]);
-#endif
 	}
-#ifdef MEM_DEBUG
-	printf("\n");
-#endif
-
 }
 
 void free_matrix(int **matrix, int n, int m)
 {
 	int i;
-#ifdef MEM_DEBUG
-	printf("\n");
-#endif
 	for (i=0; i<n; i++) {
 		free(matrix[i]);
-#ifdef MEM_DEBUG
-		printf("%d ", matrix[i]);
-#endif
 	}
-#ifdef MEM_DEBUG
-	printf("\n%d\n", matrix);
-#endif
 	free(matrix);
 }
 
@@ -116,26 +91,13 @@ int main()
 	int **grid;
 	alloc_matrix(&grid, n, m);
 
-#ifdef DEBUG
-	printf("%d %d\n", n, m);
-#endif
-
 	for (i=0; i<n; i++) {
 		for (j=0; j<m; j++) {
 			scanf("%d", grid[i]+j);
-#ifdef DEBUG
-			printf("%d ", grid[i][j]);
-#endif
 		}
-#ifdef DEBUG
-		printf("\n");
-#endif
 	}
 
 	printf("%d", get_cont(grid, n, m));
-#ifdef DEBUG
-	print_matrix(grid, n, m);
-#endif
 	free_matrix(grid, n, m);
 
 	return 0;
