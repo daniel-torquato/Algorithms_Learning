2c8b368b445003121000db1494b1735f4d64eac0
Daniel A. Torquato <daniel.torquato@icomp.ufam.edu.br>
2019-11-02 13:50:04 -0400

    Add column count

--- C/Continous_Row_Binary/src/main.c
+++ C/Continous_Row_Binary/src/main.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-int get_count_in_row(int **grid, int i, int m)
+int get_count_in_col(int **grid, int j, int n)
 {
-	int j;
+	int i;
 	int len=0, val=0;
-	for (j=0; j<m; j++) {
+	for (i=0; i<n; i++) {
 #ifdef MEM_DEBUG
 		printf("%d ", grid[i]+j);
 #endif
-
 		if (grid[i][j]) {
 			val++;
 		} else {
@@ -22,7 +20,37 @@ int get_count_in_row(int **grid, int i, int m)
 		printf(">%d ", grid[i][j], len);
 #endif
 	}
+#ifdef DEBUG
 	printf("\n");
+#endif
+	if (val>1)
+		len++;
+
+	return len;
+}
+
+int get_count_in_row(int **grid, int i, int m)
+{
+	int j;
+	int len=0, val=0;
+	for (j=0; j<m; j++) {
+#ifdef MEM_DEBUG
+		printf("%d ", grid[i]+j);
+#endif
+		if (grid[i][j]) {
+			val++;
+		} else {
+			if (val>1)
+				len++;
+			val=0;
+		}
+#ifdef DEBUG
+		printf(">%d ", grid[i][j], len);
+#endif
+	}
+#ifdef DEBUG
+	printf("\n");
+#endif
 	if (val>1)
 		len++;
 
@@ -31,9 +59,11 @@ int get_count_in_row(int **grid, int i, int m)
 int get_cont(int **grid, int n, int m)
 {
 	int ret=0;
-	int i;
+	int i, j;
 	for(i=0; i<n; i++)
 		ret+=get_count_in_row(grid, i, m);
+	for(j=0; j<m; j++)
+		ret+=get_count_in_col(grid, j, n);
 
 	return ret;
 }
@@ -57,11 +87,12 @@ void alloc_matrix (int ***matrix, int n, int m)
 
 }
 
-
 void free_matrix(int **matrix, int n, int m)
 {
 	int i;
+#ifdef MEM_DEBUG
 	printf("\n");
+#endif
 	for (i=0; i<n; i++) {
 		free(matrix[i]);
 #ifdef MEM_DEBUG
@@ -72,7 +103,6 @@ void free_matrix(int **matrix, int n, int m)
 	printf("\n%d\n", matrix);
 #endif
 	free(matrix);
-
 }
 
 int main()
@@ -83,6 +113,7 @@ int main()
 	scanf("%d", &m);
 	int **grid;
 	alloc_matrix(&grid, n, m);
+
 #ifdef DEBUG
 	printf("%d %d\n", n, m);
 #endif
