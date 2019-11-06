39437414aeb63a3f9baf4548c2d972c9173ebcf6
Daniel A. Torquato <daniel.torquato@icomp.ufam.edu.br>
2019-11-06 18:56:48 -0400

    Road Construction: Release

--- C/L_Road_Constrution/src/main.c
+++ C/L_Road_Constrution/src/main.c
@@ -14,85 +14,54 @@ int** alloc_array_ptr(int n)
 int** alloc_matrix_int(int n, int m)
 {
 	int i;
-	int **matrix=(int**) malloc(n*sizeof(int*));
-	for (int i = 0; i < n; i++) {
-		matrix[i] = malloc(m*sizeof(int));
-	}
+	int **matrix=alloc_array_ptr(n);
+	for (int i = 0; i < n; i++)
+		matrix[i] = alloc_array_int(m);
 	return matrix;
 }
 
 void free_array(int **matrix, int n)
 {
-	for (int i = 0; i < n; i++) {
+	for (int i = 0; i < n; i++)
 		free(matrix[i]);
-	}
 	free(matrix);
-}
-
-
-void print_matrix_int(int **index, int n, int m)
-{
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			printf("%d ", index[i][j]);
-		}
-		printf("\n");
-	}
-}
-
-
-void print_array2(int **index, int n, int *sizes)
-{
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < sizes[i]; j++) {
-			printf("%d ", index[i][j]);
-		}
-		printf("\n");
-	}
 }
 
 void print_array(int *array, int n)
 {
-	for (int i = 0; i < n; i++) {
+	for (int i = 0; i < n; i++)
 		printf("%d ", array[i]);
-	}
 	printf("\n");
 }
 
+void print_matrix_int(int **index, int n, int m)
+{
+	for (int i = 0; i < n; i++)
+		print_array(index[i], m);
+}
 
-int assign_worker(int **can, int n, int k, int worker, int *city, int **ans)
+void print_array2(int **index, int n, int *sizes)
+{
+	for (int i = 0; i < n; i++)
+		print_array(index[i], sizes[i]);
+}
+
+
+int assign_worker2city(int **can, int n, int k, int worker, int *city)
 {
 	int ret=1;
 
-#ifdef DEBUG
-	printf("lvl=%d/%d\n", worker, k);
-#endif
-	if (worker == n) {
-#ifdef DEBUG
-		printf("end\n");
-#endif
+	if (worker == n)
 		return 0;
-	}
+
 	for (int j = 0; j < n && ret; j++) {
 		if (can[worker][j] && !city[j]) {
 			city[j]=worker+1;
-			ans[worker][0]=j+1;
-			ans[worker][1]=can[worker][j];
-#ifdef DEBUG
-			printf("set\n");
-			print_array(city, n);
-#endif
-			ret=assign_worker(can, n, k, worker+1, city, ans);
 
-			if (ret) {
-#ifdef DEBUG
-				printf("unset\n");
-				print_array(city, n);
-#endif
+			ret=assign_worker2city(can, n, k, worker+1, city);
+
+			if (ret)
 				city[j]=0;
-				ans[worker][0]=0;
-				ans[worker][1]=0;
-			}
 		}
 	}
 	return ret;
@@ -108,10 +77,9 @@ int find_worker(int *c, int k, int n)
 	return ret;
 }
 
-int answer(int *a, int *m, int n, int **b, int *c, int k, int **ans)
+int map_worker_to_city(int *a, int *m, int n, int k, int **b, int *c, int **can)
 {
 	int flag=-1;
-	int **can=alloc_matrix_int(k, n);
 	for (int i = 0; i < n && flag; i++) {
 		for (int j = 0; j < m[i] && flag; j++) {
 			flag = find_worker(c, k, b[i][j]);
@@ -119,18 +87,29 @@ int answer(int *a, int *m, int n, int **b, int *c, int k, int **ans)
 				can[flag-1][i]=a[i];
 		}
 	}
+	return flag;
+}
 
+int answer(int *a, int *m, int n, int k, int **b, int *c, int **ans)
+{
+	int **can=alloc_matrix_int(k, n);
+
+	int flag=map_worker_to_city(a, m, n, k, b, c, can);
 #ifdef DEBUG
 	printf("flag=%d\n", flag);
 #endif
-	int *mask;
+	int *city;
 	if (flag) {
-		mask=alloc_array_int(n);
+		city=alloc_array_int(n);
 #ifdef DEBUG
 		print_matrix_int(can, k, n);
 #endif
-		assign_worker(can, n, k, 0, mask, ans);
-		free(mask);
+		flag = !assign_worker2city(can, n, k, 0, city);
+		for (int i = 0; i < n && flag; i++) {
+			ans[city[i]-1][0]=i+1;
+			ans[city[i]-1][1]=can[city[i]][i];
+		}
+		free(city);
 	}
 	free_array(can, k);
 	return flag;
@@ -147,18 +126,19 @@ int main(int argc, const char *argv[])
 	int *c=alloc_array_int(k);
 	int **b=alloc_array_ptr(n);
 	int **ans=alloc_matrix_int(k, 2);
+
 	for (int i = 0; i < n; i++) {
 		scanf("%d", a+i);
 		scanf("%d", m+i);
 		b[i]=alloc_array_int(m[i]);
-		for (int j = 0; j < m[i]; j++) {
+		for (int j = 0; j < m[i]; j++)
 			scanf("%d", b[i]+j);
-		}
 	}
-	for (int i = 0; i < k; i++) {
+
+	for (int i = 0; i < k; i++)
 		scanf("%d", c+i);
-	}
-	if (answer(a, m, n, b, c, k, ans))
+
+	if (answer(a, m, n, k, b, c, ans))
 		print_matrix_int(ans, k, 2);
 	else
 		printf("-1\n");
